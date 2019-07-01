c9c50f2ba1ff777158f4b45017611b85d2378e23
Alexander Böhn <fish2000@gmail.com>
2019-07-01 18:19:24 -0400

    Trimmed inline assert from clu.dicts

--- clu/dicts.py
+++ clu/dicts.py
@@ -55,5 +55,3 @@ def asdict(thing):
 
 # Assign the modules’ `__all__` and `__dir__` using the exporter:
 __all__, __dir__ = exporter.all_and_dir()
-
-# assert frozenset(__all__) == frozenset(('merge_two', 'merge_as', 'merge', 'asdict'))

8dbcd4a3764ed1a54204bdb168d04c5f23213447
Alexander Böhn <fish2000@gmail.com>
2019-07-01 15:28:21 -0400

    Further clu.predicates trimming

--- clu/predicates.py
+++ clu/predicates.py
@@ -236,5 +236,3 @@ export(isslotdicty,     name='isslotdicty',     doc="isslotdicty(thing) → bool
 
 # Assign the modules’ `__all__` and `__dir__` using the exporter:
 __all__, __dir__ = exporter.all_and_dir()
-
-# __dir__ = lambda: list(__all__)

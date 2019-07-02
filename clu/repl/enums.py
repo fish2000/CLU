01eb68d697ebfb8e08f4ce6bda5e5947088eb369
Alexander Böhn <fish2000@gmail.com>
2019-07-01 23:23:44 -0400

    Whitespace

--- clu/repl/enums.py
+++ clu/repl/enums.py
@@ -134,6 +134,5 @@ class AliasingEnum(Enum, metaclass=AliasingEnumMeta):
 export(DUNDER,          name='DUNDER')
 export(SUNDER,          name='SUNDER')
 
-
 # Assign the modules’ `__all__` and `__dir__` using the exporter:
 __all__, __dir__ = exporter.all_and_dir()

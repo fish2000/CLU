ec3d8b1e7e99bd90e00f6e3a56582fa89f986b2a
Alexander Böhn <fish2000@gmail.com>
2019-07-01 21:42:39 -0400

    Pruned unused SYSTEM import from test_fs_appdirectories.py

--- tests/test_fs_appdirectories.py
+++ tests/test_fs_appdirectories.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 import pytest
 
 from clu.constants import System
-# from clu.constants import SYSTEM
 from clu.fs import AppDirs, Directory
 from clu.version import VersionInfo
 

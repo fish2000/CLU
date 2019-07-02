bf8180cd35b9a6ee028a0ce18866c33ca8dfe5ec
Alexander Böhn <fish2000@gmail.com>
2019-07-01 23:32:50 -0400

    Trimmed inline assert from clu.repl.ansi

--- clu/repl/ansi.py
+++ clu/repl/ansi.py
@@ -253,7 +253,3 @@ export(Weight)
 
 # Assign the modules’ `__all__` and `__dir__` using the exporter:
 __all__, __dir__ = exporter.all_and_dir()
-
-# assert frozenset(__all__) == frozenset(('print_separator',
-#            'Text', 'Weight', 'Background', 'ANSI', 'ANSIBase', 'ANSIFormat',
-#            'print_ansi', 'print_ansi_centered', 'highlight'))

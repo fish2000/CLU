851c6f441d58a1f5c6e0062cb18bcb193efd69ad
Alexander Böhn <fish2000@gmail.com>
2019-07-22 20:53:18 -0400

    Trimmed dead code

--- clu/version/__init__.py
+++ clu/version/__init__.py
@@ -133,16 +133,11 @@ class VersionInfo(VersionAncestor):
             return type(self).NULL_VERSION
         SEPARATORS = type(self).SEPARATORS
         out = f"{self.major or 0}{SEPARATORS[0]}{self.minor or 0}"
-        # out = "%i%s%i" % (self.major or 0, SEPARATORS[0],
-        #                   self.minor or 0)
         if self.patch is not None:
-            # out += "%s%i" % (SEPARATORS[1], self.patch)
             out += f"{SEPARATORS[1]}{self.patch}"
             if self.pre:
-                # out += "%s%s" % (SEPARATORS[2], self.pre)
                 out += f"{SEPARATORS[2]}{self.pre}"
                 if self.build:
-                    # out += "%s%i" % (SEPARATORS[3], self.build)
                     out += f"{SEPARATORS[3]}{self.build}"
         return out
     

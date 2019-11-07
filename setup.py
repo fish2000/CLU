f6c9337fcb6e836226bc95c540733a0f92493a08
Alexander Böhn <fish2000@gmail.com>
2019-11-07 18:28:40 -0500

    Bump version: 0.4.0 → 0.4.1

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ try:
             '__version__.py')).read(),
             '__version__.py', 'exec'))
 except:
-    __version__ = '0.4.0'
+    __version__ = '0.4.1'
 
 # PROJECT DESCRIPTION
 LONG_DESCRIPTION = project_content('ABOUT.md')

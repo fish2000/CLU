41cff221483db376a59b88ab2df0a9fdfcb97512
Alexander Böhn <fish2000@gmail.com>
2019-11-11 13:27:55 -0500

    Migrated “clu.extensible” inline tests to the testsuite

--- tests/test_extending.py
+++ tests/test_extending.py
@@ -5,6 +5,89 @@ class TestExtending(object):
     
     """ Run the tests for extensible and pair types. """
     
+    def test_doubledutch(self):
+        from clu.extending import doubledutch
+        
+        @doubledutch
+        def yodogg(x, y):
+            return None
+        
+        @yodogg.domain(int, int)
+        def yodogg(x, y):
+            return f"INTS: {x}, {y}"
+        
+        @yodogg.domain(str, str)
+        def yodogg(x, y):
+            return f"STRS: {x}, {y}"
+        
+        @yodogg.annotated
+        def yodogg(x: float, y: float):
+            return f"FLTS: {x}, {y}"
+        
+        assert yodogg(10, 20).startswith('INTS')
+        assert yodogg('yo', 'dogg').startswith('STRS')
+        assert yodogg(3.14, 2.78).startswith('FLTS')
+        assert yodogg(object(), object()) is None
+    
+    def test_extend_pair_pairtype(self):
+        from clu.extending import pairtype, pair
+        
+        class __extend__(pairtype(int, int)):
+            
+            __name__ = "Coordinate"
+            
+            def ratio(xy):
+                x, y = xy
+                return x / y
+        
+        assert pair(2, 3).ratio() == 2 / 3
+        
+        class __extend__(pairtype(str, str)):
+            
+            __name__ = "NamespacedKey"
+            
+            def pack(tup):
+                ns, key = tup
+                return f"{ns}:{key}"
+        
+        assert pair('yo', 'dogg').pack() == "yo:dogg"
+    
+    def test_pairmro0(self):
+        from clu.config.abc import FlatOrderedSet as FOSet
+        from clu.config.abc import NamespacedMutableMapping as NaMutMap
+        from clu.extending import pairmro
+        from itertools import chain, product as dot_product
+        
+        iterchain = chain.from_iterable
+        
+        pair = []
+        product = []
+        
+        for tup in pairmro(FOSet, NaMutMap):
+            pair.append(tuple(str(el) for el in tup))
+        
+        for itp in dot_product(FOSet.__mro__, NaMutMap.__mro__):
+            product.append(tuple(str(el) for el in itp))
+        
+        sorter = lambda t: ''.join(iterchain(t))
+        retros = lambda t: ''.join(reversed(list(iterchain(t))))
+        
+        assert sorted(pair, key=sorter) == sorted(product, key=sorter)
+        assert sorted(pair, key=retros) == sorted(product, key=retros)
+        assert sorted(pair) == sorted(product)
+        assert pair == product
+    
+    def test_pairmro1(self):
+        from clu.config.abc import FlatOrderedSet as FOSet
+        from clu.config.abc import NamespacedMutableMapping as NaMutMap
+        from clu.extending import pairmro
+        from itertools import product as dot_product
+        
+        pairs    = list(pairmro(FOSet, NaMutMap))
+        products = list(dot_product(FOSet.mro(), NaMutMap.mro()))
+        
+        assert set(pairs) == set(products) # REALLY.
+    
     def test_extensible_metaclass(self, dirname):
         from clu.constants.consts import PROJECT_NAME, TEST_PATH
         from clu.extending import Extensible

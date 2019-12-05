259873a07177cc1c7a1f0ff041a0c8ec442f60f5
Alexander Böhn <fish2000@gmail.com>
2019-12-04 21:09:26 -0500

    Further fleshing out REPL script

--- clu/repl/__init__.py
+++ clu/repl/__init__.py
@@ -1,2 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
+
+# Set up some “example” instances –
+# I frequently create trivial lists, strings, tuples etc.,
+# to be used as exemplars from which I can check methods and
+# suchlike using the interactive interpreter:
+b = b'yo dogg'
+B = bytearray(b)
+m = memoryview(b)
+c = complex(0, 1)
+d = { 'yo' : "dogg" }
+t = ('yo', 'dogg')
+l = ['yo', 'dogg']
+S = { 'yo', 'dogg' }
+F = frozenset(S)
+f = 0.666
+i = 666
+o = object()
+s = 'yo dogg'
--- clu/scripts/repl.py
+++ clu/scripts/repl.py
@@ -1,6 +1,11 @@
 # -*- encoding: utf-8 -*-
 from __future__ import print_function
 
+from clu.constants import consts
+from clu.all import import_clu_modules
+from clu.naming import dotpath_split, qualified_import, qualified_name
+from clu.typespace.namespace import Namespace
+
 def star_export(module, namespace=None):
     """ Safely bind everything a module exports to a namespace. """
     if namespace is None:
@@ -8,10 +13,12 @@ def star_export(module, namespace=None):
     for name in dir(module):
         namespace[name] = getattr(module, name)
 
-from clu.constants import consts
-from clu.all import import_clu_modules
-from clu.naming import dotpath_split, qualified_import
-from clu.typespace.namespace import Namespace
+def module_export(module, namespace=None):
+    """ Safely bind a module to a namespace. """
+    if namespace is None:
+        return
+    name = dotpath_split(qualified_name(module))[0]
+    namespace[name] = module
 
 modules = import_clu_modules()
 namespace = Namespace(**modules)
@@ -19,36 +26,19 @@ namespace = Namespace(**modules)
 star_export(consts, namespace=globals())
 # star_export(namespace, namespace=globals())
 
-starmods = ('clu.dicts', 'clu.enums', 'clu.exporting', 'clu.extending',
-            'clu.importing', 'clu.naming', 'clu.predicates', 'clu.typology')
+starmods = ('clu.repl',
+            'clu.dicts', 'clu.enums', 'clu.exporting', 'clu.extending',
+            'clu.importing', 'clu.naming', 'clu.predicates', 'clu.typology',
+            'clu.fs.filesystem', 'clu.fs.misc')
 
-mods = ('clu.all', 'clu.dispatch', 'clu.sanitizer')
+mods = ('clu.all', 'clu.dispatch', 'clu.sanitizer', 'clu.fs.pypath',
+        'clu.typespace.types')
 
 for starmod in starmods:
     star_export(qualified_import(starmod), namespace=globals())
 
 for mod in mods:
-    module = qualified_import(mod)
-    name = dotpath_split(mod)[0]
-    globals()[name] = module
-
-# Set up some “example” instances –
-# I frequently create trivial lists, strings, tuples etc.,
-# to be used as exemplars from which I can check methods and
-# suchlike using the interactive interpreter:
-b = b'yo dogg'
-B = bytearray(b)
-m = memoryview(b)
-c = complex(0, 1)
-d = { 'yo' : "dogg" }
-t = ('yo', 'dogg')
-l = ['yo', 'dogg']
-S = { 'yo', 'dogg' }
-F = frozenset(S)
-f = 0.666
-i = 666
-o = object()
-s = 'yo dogg'
+    module_export(qualified_import(mod), namespace=globals())
 
 # Remove duplicate and invalid sys.paths:
 from clu.fs.pypath import remove_invalid_paths

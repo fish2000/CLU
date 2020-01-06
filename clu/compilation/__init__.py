f064b7d8ee51cb541961dfe26263d46ec4f191c9
Alexander Böhn <fish2000@gmail.com>
2020-01-06 18:43:34 -0500

    Removed intermediate package imports from “clu.compilation”

--- clu/compilation/__init__.py
+++ clu/compilation/__init__.py
@@ -1,10 +1,2 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-
-from .compiledb import (CDBSubBase, CDBBase, CDBJsonFile)
-from .macros import (Macro, Macros)
-
-__all__ = ('Macro', 'Macros',
-           'CDBSubBase', 'CDBBase', 'CDBJsonFile')
-
-__dir__ = lambda: list(__all__)

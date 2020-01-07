cf00675cd7ff880aef2c4902498a3cac6c092113
Alexander Böhn <fish2000@gmail.com>
2020-01-07 11:39:57 -0500

    Import order matters

--- clu/testing/pytest.py
+++ clu/testing/pytest.py
@@ -28,6 +28,8 @@ def pytest_sessionfinish(session, exitstatus):
     from clu.constants import consts
     from clu.dispatch import exithandle
     from clu.fs.filesystem import td, rm_rf
+    from clu.repl import ansi
+    from clu.scripts.ansicolors import yellow
     
     # putative temporary directory:
     putative = td().subdirectory(f"pytest-of-{consts.USER}")
@@ -40,8 +42,6 @@ def pytest_sessionfinish(session, exitstatus):
         @exithandle
         def remover(signum, frame=None):
             """ Exit handler for removing ‘pytest’ artifacts """
-            from clu.scripts.ansicolors import yellow
-            from clu.repl import ansi
             if putative.exists:
                 message = f"removing: “$TMPDIR/{putative.basename}”"
                 ansi.print_ansi_centered(message, color=yellow, filler='-')

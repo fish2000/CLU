eb4da9ef1beeb4099ddf7ad37a735c4b01cf495e
Alexander Böhn <fish2000@gmail.com>
2020-01-07 06:22:38 -0500

    Running all inline test suites automatically via Nox ... !!!!!!!!!!!!!!!!!!!

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,8 @@
 from __future__ import print_function
 
 import nox
+import sys, os
+sys.path.append(os.path.dirname(__file__))
 
 # @nox.session(python=['3.7', '3.8', 'pypy3'])
 @nox.session
@@ -14,8 +16,22 @@ def pytest(session):
     session.run('pytest')
 
 @nox.session
-def check_clu_basics(session):
+def check_clu_modules(session):
     """ Check CLU constants and module exports """
     session.install("-r", "requirements/install.txt")
     session.run('python', '-m', 'clu.constants')
     session.run('python', '-m', 'clu')
+    
+    from clu.all import import_clu_modules
+    from clu.predicates import resolve
+    clumods = import_clu_modules()
+    
+    for dotpath, module in clumods.items():
+        test_fn = resolve(module, 'test')
+        if test_fn is not None:
+            if callable(test_fn):
+                names = resolve(test_fn, '__code__.co_names')
+                if names is not None:
+                    if 'inline' in names:
+                        session.run('python', '-m', dotpath,
+                           env=dict(SYSTEMROOT='wat'))

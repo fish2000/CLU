295eb3ff4376db45ac3bc7a662e577f9f1e93609
Alexander Böhn <fish2000@gmail.com>
2020-01-14 20:10:31 -0500

    Setting the CLU-specific pytest options in “conftest.py” ... using “Config.addinivalue_line(…)” in the “pytest_configure”     hook function

--- conftest.py
+++ conftest.py
@@ -3,7 +3,22 @@
 # Load the CLU plugin for `pytest`:
 pytest_plugins = "clu.testing.pytest"
 
+# Directories to exclude (using “norecursedirs”):
+excludes = ('.git', '.svn', '.hg', '.tox', 'CVS',
+            '*.egg', '*.egg-info',
+            '*.',
+            'build', 'bdist', 'dist', 'sdist',
+            'venv', 'develop')
+
+import pytest
+
 # The same as passing “--delete-temporary”:
 def pytest_delete_temporary_default():
     """ Delete all “$TMPDIR/pytest-of-$USER” files by default """
     return True
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config):
+    """ Add the configuration options for CLU’s test suite """
+    config.addinivalue_line('testpaths',        "tests")
+    config.addinivalue_line('norecursedirs',    " ".join(excludes))

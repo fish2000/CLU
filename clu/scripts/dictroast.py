2a63d98b000515a281e26c57bdd89fc08c038ae5
Alexander Böhn <fish2000@gmail.com>
2022-10-03 01:35:12 -0400

    Initial start of a command-line test/analysis script

---
+++ clu/scripts/dictroast.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import shlex
+import sys, os
+
+from pprint import pprint
+
+from clu.scripts import treeline
+from clu.predicates import listify
+from clu.exporting import Exporter
+
+exporter = Exporter(path=__file__)
+export = exporter.decorator()
+
+valid_actions = { 'read', 'write', 'status' }
+
+def get_dict():
+    """ Retrieve a copy of the database for this file """
+    with exporter.data() as database:
+        out = dict(database.get('tree', {}))
+    return out
+
+def update_dict(updates):
+    """ Update the files’ database from a new dictionary """
+    with exporter.data() as database:
+        if 'tree' not in database:
+            database['tree'] = {}
+        database['tree'].update(updates)
+        out = dict(database.get('tree', {}))
+    return out
+
+def get_command_history():
+    """ Retrieve a copy of the command history for this file """
+    with exporter.data() as database:
+        out = listify(database.get('history', []))
+    return out
+
+def push_command(command):
+    """ Add a new command to the end of the files’ command history """
+    with exporter.data() as database:
+        if 'history' not in database:
+            database['history'] = []
+        database['history'].append(command)
+        out = listify(database.get('history', []))
+    return out
+
+def _initialize():
+    """ Set up the database with empty entries for the command tree
+        and the history stack. Only call this once: when running
+        the script for the first time!!
+    """
+    with exporter.data() as database:
+        database['tree'] = {}
+        database['history'] = []
+        out = dict(database)
+    return out
+
+def split_arguments(argv=None):
+    """ Return the script arguments, in the form:
+        
+        >>> executable, action, *nsflags = shlex.split(command)
+    """
+    if not argv:
+        argv = sys.argv
+    return argv
+
+def join_arguments(argv=None):
+    """ Return the script arguments, joined as a single string """
+    if not argv:
+        argv = sys.argv
+    return shlex.join(argv)
+
+def flags_to_nodetreemap(*flags):
+    """ Parse script flag arguments into a node tree, and store
+        them in a new NodeTreeMap instance
+    """
+    root = treeline.RootNode()
+    root.populate_with_arguments(*flags)
+    return treeline.NodeTreeMap(tree=root)
+
+def main(argv=None):
+    """ The primary script entry point """
+    # Default to `sys.argv` if we’re called without any
+    # arguments specified:
+    if not argv:
+        argv = sys.argv
+    
+    # Ensure the argument vector is long enough:
+    if len(argv) < 2:
+        argv.append('NOP')
+    
+    # Initialize, if necessary:
+    # _initialize()
+    
+    # Deal with the command:
+    command = join_arguments(argv=argv)
+    executable, action, *nsflags = split_arguments(argv=argv)
+    ntm = flags_to_nodetreemap(*nsflags)
+    
+    # Print current command:
+    print("THIS COMMAND:")
+    print(command)
+    print()
+    
+    # Print parsed command dict:
+    for line in treeline.tree_repr(ntm.tree, treeline.Level()):
+        print(line)
+    print()
+    
+    # Print command history:
+    print("COMMAND HISTORY:")
+    # pprint(get_command_history(), indent=4)
+    for idx, line in enumerate(get_command_history()):
+        print(f"• {idx} {line}")
+    print()
+    
+    if action.casefold() not in valid_actions:
+        actions = ", ".join(sorted(map(lambda term: term.upper(), valid_actions)))
+        print(f"¶ INVALID ACTION: “{action}”")
+        print(f"¶ MUST BE ONE OF: «{actions}»")
+        return os.EX_CONFIG
+    
+    if action.casefold() == 'write':
+        # Update the dict:
+        print("… UPDATING DICTIONARY FROM COMMAND")
+        update_dict(ntm)
+    elif action.casefold() == 'read':
+        # Print specified keys:
+        keys = ", ".join(ntm.keys())
+        print(f"… READING DICTIONARY KEYS: «{keys}»")
+    elif action.casefold() == 'status':
+        # Do something status-y:
+        pass
+    
+    print()
+    
+    # Update the command history:
+    push_command(command)
+    
+    # Print current dict:
+    pprint(get_dict())
+    print()
+    
+    # Exit cleanly:
+    return os.EX_OK
+
+# Assign the modules’ `__all__` and `__dir__` using the exporter:
+__all__, __dir__ = exporter.all_and_dir()
+
+def test():
+    
+    from clu.testing.utils import inline
+    
+    @inline
+    def test_one():
+        pass # INSERT TESTING CODE HERE, pt. I
+    
+    #@inline
+    def test_two():
+        pass # INSERT TESTING CODE HERE, pt. II
+    
+    #@inline.diagnostic
+    def show_me_some_values():
+        pass # INSERT DIAGNOSTIC CODE HERE
+    
+    return inline.test(100)
+
+test_command = "clu-command READ " \
+               "--key0 --key1 --key2 ns0 " \
+               "--key3 ns1 ns2 " \
+               "--key4 --key5"
+
+test_command = "clu-command WRITE " \
+               "--key0=yo --key1=dogg --key2=i_heard ns0 " \
+               "--key3=you_like ns1 ns2 " \
+               "--key4=tree --key5=structures"
+
+# test_command = "clu-command STATUS"
+# test_command = "clu-command FAIL"
+
+if __name__ == '__main__':
+    # sys.exit(test())
+    # sys.exit(main(shlex.split(test_command)))
+    sys.exit(main(sys.argv))

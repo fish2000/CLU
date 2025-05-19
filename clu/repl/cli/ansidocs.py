3b4ccc9c400e28109dd548c90b04561800691cbb
Alexander Böhn <fish2000@gmail.com>
2025-05-19 15:57:38 -0400

    The ANSI docstring CLU command seems to be up to snuff

--- clu/repl/cli/ansidocs.py
+++ clu/repl/cli/ansidocs.py
@@ -7,6 +7,9 @@ import sys
 import shlex
 
 def actually_print_ansidocs(dotpath):
+    """ This function prints the ANSI docs directly to standard output,
+        formatting accordingly (as in, no ANSI if we can’t ANSI)
+    """
     from clu.naming import qualified_import
     from clu.repl.ansi import DocFormat
     from importlib import import_module
@@ -21,27 +24,26 @@ def actually_print_ansidocs(dotpath):
     ansidocs(thing)
 
 def ansidocs_command():
-    """ Print out the ANSI-ified docstring for the given thing"""
-    from clu.scripts.treeline import RootNode, NodeTreeMap
+    """ Print out the ANSI-ified docstring for the given thing(s) """
+    from clu.constants import consts
     
     command = " ".join(sys.argv)
     executable, *dotpaths = shlex.split(command)
     
-    # root = RootNode.populate(*dotpaths)
-    # nodemap = NodeTreeMap(tree=root)
-    # namespaces = tuple(nodemap.namespaces())
-    # items = tuple(nodemap.items())
-    # print(f"NAMESPACES: {namespaces}")
-    # print(f"NODEMAP THINGS: {items}")
+    # If we’re debuggin’ lets’s talk about what we got:
+    if consts.DEBUG:
+        alldotpaths = " ".join(dotpaths)
+        print(f"EXECUTABLE: {executable}")
+        print(f"DOTPATHS: {alldotpaths}")
     
-    alldotpaths = " ".join(dotpaths)
-    print(f"EXECUTABLE: {executable}")
-    print(f"DOTPATHS: {alldotpaths}")
+    # The actual dirty work
+    for dotpath in dotpaths:
+        actually_print_ansidocs(dotpath)
     
     # Return nice-nice for my POSI(X)ES:
     return os.EX_OK
 
 if __name__ == '__main__':
-    sys.argv.append('clu.exporting.Exporter')
-    sys.argv.append('clu.repl.ansi.Text')
+    # sys.argv.append('clu.exporting.Exporter')
+    # sys.argv.append('clu.repl.ansi.Text')
     sys.exit(ansidocs_command())

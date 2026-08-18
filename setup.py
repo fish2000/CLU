f6f114f1963850e077b4b4e437ee43e6df04c360
Alexander Böhn <fish2000@gmail.com>
2026-08-18 02:21:14 -0400

    Bumped version: 0.12.23 → 0.12.24

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ try:
             '__version__.py')).read(),
             '__version__.py', 'exec'))
 except:
-    __version__ = '0.12.23'
+    __version__ = '0.12.24'
 
 # PROJECT DESCRIPTION
 LONG_DESCRIPTION = project_content('ABOUT.md')
